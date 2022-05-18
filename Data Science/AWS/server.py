96f066890a56d4ffc030af88c952948b5f207946
Peng Zhang <zhangpeng4242@gmail.com>
2022-05-18 15:00:49 +0300

    Peng-make_img_url_work

--- Data Science/AWS/server.py
+++ Data Science/AWS/server.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import request, jsonify
-from PIL import Image
-from flask import send_file
 import os
+from flask import send_file
 
 
 def make_userimg_dir():
     if not os.path.exists("user_images/"):
         os.mkdir("user_images")
+    if not os.path.exists("user_images_good/"):
+        os.mkdir("user_images_good")
+    if not os.path.exists("user_images_bad/"):
+        os.mkdir("user_images_bad")
 
 
 app = Flask(__name__)
@@ -21,12 +24,19 @@ def predict_age():
 
     response_json = jsonify(response={
         "message": f"Image {imagefile.filename} has been successfully save to path {os.getcwd() + f_path}!!",
-        "good_img_url": f"{os.getcwd() + f_path}",
-        "bad_img_url": f"{os.getcwd() + f_path}"
+        "good_img_url": f"/get_image?fn={imagefile.filename}&good=1",
+        "bad_img_url": f"/get_image?fn={imagefile.filename}&good=0"
     })
     return response_json
 
 
+@app.route("/get_image")
+def get_image():
+    img_genre = request.args.get("good")
+    img_name = request.args.get("fn")
+    return send_file(f"user_images/{img_name}", mimetype='image/gif')
+
+
 if __name__ == "__main__":
     make_userimg_dir()
-    app.run(port=8080)
+    app.run(host="0.0.0.0", port=8080)
