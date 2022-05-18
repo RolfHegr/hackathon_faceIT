e3524612f871288a5a0f67f77fca03ec4f0718a3
Peng Zhang <zhangpeng4242@gmail.com>
2022-05-18 12:46:19 +0300

    Peng return image

--- Data Science/AWS/server.py
+++ Data Science/AWS/server.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask import request, jsonify
+from PIL import Image
+from flask import send_file
+import os
+
+
+def make_userimg_dir():
+    if not os.path.exists("user_images/"):
+        os.mkdir("user_images")
+
 
 app = Flask(__name__)
-@app.route("/predict_age")
+
+
+@app.route("/predict_age", methods=["GET", "POST"])
 def predict_age():
-    return "You have successfully connected to the server. AWS"
+    imagefile = request.files['img_file']
+    f_path = 'user_images/' + imagefile.filename
+    imagefile.save(f_path)
+
+    response_json = jsonify(response={
+        "message": f"Image {imagefile.filename} has been successfully save to path {os.getcwd() + f_path}!!",
+        "good_img_url": f"{os.getcwd() + f_path}",
+        "bad_img_url": f"{os.getcwd() + f_path}"
+    })
+    return response_json
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
+    make_userimg_dir()
+    app.run(port=8080)
