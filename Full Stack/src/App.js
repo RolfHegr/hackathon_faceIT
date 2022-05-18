740ea79f3e0d772ae5188cce1d04ff58b533d507
Yosef Semionov <yosefs283@gmail.com>
2022-05-18 13:36:39 +0300

    small changes

--- Full Stack/src/App.js
+++ Full Stack/src/App.js
@@ -5,12 +5,7 @@ import WelcomePage from "./components/welcomePage/WelcomePage";
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-    <WelcomePage />
-    {/* <Form/> */}
-=======
-      <Form/> 
->>>>>>> fc305ef2ca698e6cc0d676348c1740e0cbdeade6
+      <WelcomePage/>
     </div>
   )
 }
