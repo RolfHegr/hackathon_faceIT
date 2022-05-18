7058dc5b4172d21ad770335b8bf3035f09bda26b
garman000 <garman000@gmail.com>
2022-05-18 16:38:57 +0300

    results page done - waiting for DS

--- Full Stack/src/components/resultsPage/ResultsPage.jsx
+++ Full Stack/src/components/resultsPage/ResultsPage.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import "./resultsPage.css";
 import face from "../welcomePage/images/face.jpg";
+import react from "react";
 
 const ResultsPage = () => {
+  const [showMore, setShowMore] = useState(true);
+
   return (
     <Container>
       <div className="resultHeader">
-        <h1>This is You</h1>
+        <h1>Welcome to the future you</h1>
       </div>
       <div className="resultCard">
-          <div className="resultImage">
-              <img src={face} />
-          </div>
+        <div className="resultImage">
+          <img src={face} width="500" height="500" />
+          <button
+            className="resultBtn"
+            onClick={() => {
+              setShowMore(!showMore);
+            }}
+          >
+            {showMore ? "Good You" : "Future Me"}
+          </button>
+        </div>
       </div>
-
     </Container>
   );
 };
